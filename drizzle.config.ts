237dcdcb21a76f25ccfc3bdb88198599352075f0
polartechatendimentos-beep <polartechatendimentos@gmail.com>
2026-08-03 14:11:42 -0300

    Atualiza projeto corrigido ProAR

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./db/schema.ts",
   out: "./drizzle",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.POSTGRES_URL || "",
-  },
+  schema: "./db/schema.ts",
+  dialect: "sqlite",
 });
