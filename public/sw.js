ff9fbc1c1072f3ae7d7bfaab5632b6844a94d648
polartechatendimentos-beep <polartechatendimentos@gmail.com>
2026-08-10 10:16:38 -0300

    Atualiza cache offline do aplicativo móvel

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "proar-shell-v1";
+const CACHE = "proar-shell-v2";
 const SHELL = ["/", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png", "/proar-logo.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
